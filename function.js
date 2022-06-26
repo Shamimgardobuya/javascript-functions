be6207069c017fcab07f930c0f33ceda406815c6
Shamimgardobuya <shamimobuya@gmail.com>
2022-06-26 07:23:05 +0300

    used functions

---
+++ function.js
@@ -0,0 +1,25 @@
+// create a programme in js that greets people with their name
+// create a function
+// nside the function body using thhe prompt function in your console,request for users na,e
+//use backticks together with console and dollar sign and greet the person
+//nb function should be descriptive
+//call the function
+function greetings(){
+    let namep="klaus";
+    console.log(`Hello ${namep}`);
+}
+greetings()
+console.log("Hello");
+    // function parameters
+// create a programme that finds the difference between two numbers
+// create function and pass in two parameters
+//using console.log print the differrence using the- operator
+// call the function and pass in the two values 
+function subtract(numb1,numb2){
+    console.log(numb1-numb2);
+
+}
+subtract(8,12)
+
+
+
